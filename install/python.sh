96ae086a4df0fd62f2806c9183ff33791f5bd9de
Philip Harrison <philip@mailharrison.com>
2018-03-22 22:24:04 +0100

    Install Python 3

---
+++ install/python.sh
@@ -0,0 +1,13 @@
+# https://jacobian.org/writing/python-environment-2018/
+
+pyenv install 3.6.4
+pyenv global 3.6.4
+
+curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
+
+pipsi install pew
+pipsi install pipenv
+
+pip install virtualenv
+
+pipsi install awscli
