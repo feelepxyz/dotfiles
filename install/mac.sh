dae2447870fa226dcf8d790349ea33f1b68e663f
Philip Harrison <philip@mailharrison.com>
2018-10-21 14:53:24 +0200

    Moar mac config

--- install/mac.sh
+++ install/mac.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+touch ~/.hushlogin
+
 # Set up Safari for development.
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
@@ -10,6 +16,8 @@ defaults write -g WebKitDeveloperExtras -bool true
 # Expand print panel by default
 defaults write -g PMPrintingExpandedStateForPrint -bool true && \
 defaults write -g PMPrintingExpandedStateForPrint2 -bool true
+
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 # Expand save panel by default
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true && \
@@ -25,7 +33,10 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
 # Enable firewall
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+sudo defaults write /Library/Preferences/com.apple.alf allowsignedenabled -bool false
+sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 1
+sudo defaults write /Library/Preferences/com.apple.alf loggingenabled -bool true
+sudo defaults write /Library/Preferences/com.apple.alf stealthenabled -bool true
 
 # Password on screensaver after 1 minute
 defaults write com.apple.screensaver askForPassword -int 1
@@ -57,6 +68,10 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
+# ActivityMonitor
+defaults write com.apple.ActivityMonitor IconType -int 5
+defaults write com.apple.ActivityMonitor ShowCategory -int 100
+
 # Enable AirDrop over Ethernet and on unsupported Macs running Lion
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
@@ -72,7 +87,12 @@ defaults write com.apple.iTunes disablePingSidebar -bool true
 # Disable all the other Ping stuff in iTunes
 defaults write com.apple.iTunes disablePing -bool true
 
+# Enable tap to click
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
 # Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
 
 # Set language and text formats
@@ -98,9 +118,68 @@ defaults write com.apple.terminal StringEncodings -array 4
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # Set computer name/host name
-sudo scutil --set ComputerName "feelepxyz.local" && \
-sudo scutil --set HostName "feelepxyz.local" && \
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "feelepxyz.local"
+sudo hostname -s feelepxyz
+sudo scutil --set ComputerName feelepxyz
+sudo scutil --set LocalHostName feelepxyz
+sudo scutil --set HostName feelepxyz
+sudo sysctl kern.hostname=feelepxyz
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "feelepxyz"
+echo "kern.hostname=feelepxyz" | sudo tee -a /etc/sysctl.conf
+
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+defaults write com.apple.appstore ShowDebugMenu -bool true
+
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
+defaults write com.apple.DiskUtility advanced-image-options -bool true
+
+defaults write com.apple.TextEdit RichText -int 0
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+defaults write com.apple.screencapture disable-shadow -bool true
+defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+defaults write com.apple.screencapture type -string "png"
+
+defaults write com.apple.spotlight orderedItems -array \
+  '{"enabled" = 1;"name" = "APPLICATIONS";}' \
+  '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+  '{"enabled" = 1;"name" = "DIRECTORIES";}' \
+  '{"enabled" = 1;"name" = "PDF";}' \
+  '{"enabled" = 1;"name" = "FONTS";}' \
+  '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+  '{"enabled" = 0;"name" = "MESSAGES";}' \
+  '{"enabled" = 0;"name" = "CONTACT";}' \
+  '{"enabled" = 0;"name" = "EVENT_TODO";}' \
+  '{"enabled" = 0;"name" = "IMAGES";}' \
+  '{"enabled" = 0;"name" = "BOOKMARKS";}' \
+  '{"enabled" = 0;"name" = "MUSIC";}' \
+  '{"enabled" = 0;"name" = "MOVIES";}' \
+  '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+  '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+  '{"enabled" = 0;"name" = "SOURCE";}' \
+  '{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
+  '{"enabled" = 0;"name" = "MENU_OTHER";}' \
+  '{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
+  '{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
+  '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
+  '{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+
+sudo defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0
+
+sudo defaults write /Library/Preferences/com.apple.mDNSResponder NoMulticastAdvertisements -bool true
+
+sudo defaults write /Library/Preferences/com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.captive.control Active -bool false
+
+sudo systemsetup -setusingnetworktime on
+sudo systemsetup -setnetworktimeserver time1.google.com
 
 # Enable FileVault
 fdesetup status | grep "On"
