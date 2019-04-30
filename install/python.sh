4afd0781d02ad310576c626ff126eaf041b49fa1
Philip Harrison <philip@mailharrison.com>
2019-04-30 10:54:14 +0100

    Updarte python version

--- install/python.sh
+++ install/python.sh
@@ -1,13 +1,11 @@
 # https://jacobian.org/writing/python-environment-2018/
 
-pyenv install 3.6.4
-pyenv global 3.6.4
+pyenv install 2.7
+pyenv install 3.6
+pyenv global 3.6.x
+
+pip install virtualenv
+pip install --upgrade pip
 
 curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
 
-pipsi install pew
-pipsi install pipenv
-
-pip install virtualenv
-
-pipsi install awscli
